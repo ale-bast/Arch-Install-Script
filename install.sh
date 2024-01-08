d4189ea8a207c8859c9550858972cb909fc3ac61
ale-bast <17410941+ale-bast@users.noreply.github.com>
2024-01-08 20:25:42 +0100

    added dm-setbg, modified install.sh and pkglist.txt

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-echo "#########################################################"
-echo "## Adding the DTOS core repository to /etc/pacman.conf ##"
-echo "#########################################################"
-grep -qxF "[dtos-core-repo]" /etc/pacman.conf || ( echo " "; echo "[dtos-core-repo]"; echo "SigLevel = Optional DatabaseOptional"; \ echo "Server = https://gitlab.com/dtos/\$repo/-/raw/main/\$arch") | sudo tee -a /etc/pacman.conf } addrepo || error "Error adding DTOS repo to /etc/pacman.conf."
+addrepo() { \
+    echo "#########################################################"
+    echo "## Adding the DTOS core repository to /etc/pacman.conf ##"
+    echo "#########################################################"
+    grep -qxF "[dtos-core-repo]" /etc/pacman.conf ||
+        ( echo " "; echo "[dtos-core-repo]"; echo "SigLevel = Optional DatabaseOptional"; \
+        echo "Server = https://gitlab.com/dtos/\$repo/-/raw/main/\$arch") | sudo tee -a /etc/pacman.conf
+}
+
+addrepo || error "Error adding DTOS repo to /etc/pacman.conf."
 
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf || error "Failed to uncomment multilib repository"
 sudo sed -i '/Color/ s/#//' /etc/pacman.conf
@@ -13,6 +19,8 @@ sudo pacman --noconfirm --needed -Sy dtos-core-repo/paru-bin || error "Error ins
 sudo paru --needed -Sy - cat pkglist.txt || error "Failed to install a required package from pkglist.txt."
 
 cp -Rf /etc/dtos/.local/* /home/axel/.local || error "Failed to copy /etc/dtos/.local/bin folder to home/.local/bin"
+
+sudo cp -f "$HOME/Arch-Install-Script/dm-setbg" /usr/bin/ || "Failed to replace /usr/bin/dm-setbg"
 
 find "$HOME/.local/bin" -type f -print0 | xargs -0 chmod 775 || error "Failed to change permissions of $HOME/.local/bin folder to 775"
 
