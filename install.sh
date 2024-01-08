ef5092a35e3445f2e15adc12be753b4d143e115b
ale-bast <17410941+ale-bast@users.noreply.github.com>
2024-01-08 22:43:13 +0100

    modified install.sh

--- install.sh
+++ install.sh
@@ -14,20 +14,38 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf || error "Failed
 sudo sed -i '/Color/ s/#//' /etc/pacman.conf || error "Failed to uncomment Color"
 sudo sed -i '/ILoveCandy/ s/#//' /etc/pacman.conf || error "Failed to uncomment ILoveCandy"
 
+echo "paru"
+
 sudo pacman --noconfirm --needed -Sy dtos-core-repo/paru-bin || error "Error installing dtos-core-repo/paru-bin."
 
-sudo paru --needed -Sy - cat pkglist.txt || error "Failed to install a required package from pkglist.txt."
+echo "pkglist"
+
+paru --needed -Sy - cat pkglist.txt || error "Failed to install a required package from pkglist.txt."
+
+echo "copy .local/bin"
 
 cp -Rf /etc/dtos/.local/* /home/axel/.local || error "Failed to copy /etc/dtos/.local/bin folder to home/.local/bin"
 
-sudo cp -f "$HOME/Arch-Install-Script/dm-setbg" /usr/bin/ || "Failed to replace /usr/bin/dm-setbg"
+echo "chmod"
 
 find "$HOME/.local/bin" -type f -print0 | xargs -0 chmod 775 || error "Failed to change permissions of $HOME/.local/bin folder to 775"
 
+echo "dm-setbg"
+
+sudo cp -f "$HOME/Arch-Install-Script/dm-setbg" /usr/bin/ || "Failed to replace /usr/bin/dm-setbg"
+
+echo "hooks"
+
 sudo cp /etc/dtos/.config/xmonad/pacman-hooks/* /etc/pacman.d/hooks/ || error "Failed to copy xmonad's pacman-hooks for recompilation"
+
+echo "ricemood"
 
 sudo npm install -g ricemood || error "Failed to install ricemood"
 
+echo "zsh"
+
 sudo chsh "$USER" -s "/bin/zsh" || error "Failed to change shell"
 
-sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf || error "Failed to change the number of cores used during compilation"
+echo "makepkg"
+
+sudo sed -i 's/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$(nproc)\"/g' /etc/makepkg.conf || error "Failed to change the number of cores used during compilation"
